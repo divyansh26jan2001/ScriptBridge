77568d78e91f7439cc783c99829ef1a8f0196eb0
divyansh26jan2001 <50747889+divyansh26jan2001@users.noreply.github.com>
2024-11-21 12:43:53 +0530

    Delete ScriptBridge directory

--- ScriptBridge/ScriptBridgeWebApp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import ProgrammingQuestion
-
-@admin.register(ProgrammingQuestion)
-class ProgrammingQuestionAdmin(admin.ModelAdmin):
-    list_display = ('sr_no', 'question', 'language')  # Fields to display in the admin list view
